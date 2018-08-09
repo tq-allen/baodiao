95603091a637804ddfeae019e2dd43736a275c5a
allen <531767420@qq.com>
2018-08-09 20:42:40 +0800

    完善

--- src/pages/calc.vue
+++ src/pages/calc.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="calc">
-    <h1>1</h1>
+    <h1><img src="../assets/2.jpg" alt="">宝屌赔率计算器<img src="../assets/3.jpg" alt=""></h1>
     <el-row :gutter="40">
-      <el-col :span="8">
+      <el-col :span="6">
         <el-card header="卡数">
           <el-form label-position="right" label-width="50px" :model="cards">
             <el-form-item v-for="(item, key) in cards" :label="key" :key="key">
@@ -14,26 +14,43 @@
           </el-form>
         </el-card>
       </el-col>
-      <el-col :span="16">
-        <el-table
+      <el-col :span="18">
+      <el-table
         :data="tableData"
         border
+        show-summary
         style="width: 100%">
         <el-table-column
           prop="fee"
           label="费数"
-          width="180">
+          width="120"
+          header-align="center">
+        </el-table-column>
+        <el-table-column
+          prop="num"
+          label="卡数"
+          width="120"
+          header-align="center">
         </el-table-column>
         <el-table-column
           prop="gl"
           label="概率"
-          width="180">
+          width="120"
+          header-align="center">
+        </el-table-column>
+        <el-table-column
+          prop="gl_per"
+          label="概率(%)"
+          header-align="center">
         </el-table-column>
         <el-table-column
           prop="pl"
-          label="赔率">
+          label="赔率"
+          width="120"
+          header-align="center">
         </el-table-column>
       </el-table>
+      <img style="width: 200px; margin-top: 20px;" src="../assets/1.jpg" alt="">
       </el-col>
     </el-row>
   </div>
@@ -69,10 +86,11 @@ export default {
       for (let key in this.cards) {
         if (this.cards[key] === '') continue
         console.log(key, this.cards[key])
-        let gl = (Number(this.cards[key]) / this.totalCards).toFixed(3)
-        let pl = (1 / gl).toFixed(2)
+        let gl = (parseInt(this.cards[key]) / this.totalCards).toFixed(3)
+        let glPer = (gl * 100).toFixed(2) + '%'
+        let pl = (1 / gl).toFixed(1)
 
-        this.tableData.push({'fee': key, 'gl': gl, 'pl': pl})
+        this.tableData.push({'fee': key, 'num': this.cards[key], 'gl': gl, 'gl_per': glPer, 'pl': pl})
       }
       console.log('totalCards', this.totalCards)
       console.log('totalCards', typeof (this.totalCards))
@@ -81,9 +99,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 .calc{
-  width: 800px;
-  margin: 20px auto;
+  width: 1000px;
+  margin: 0 auto;
+  h1{
+    display: flex;
+    height: 50px;
+    justify-content: center;
+    align-items: center;
+    img{
+      width: 50px;
+      margin: 0 20px;
+    }
+  }
 }
 </style>
